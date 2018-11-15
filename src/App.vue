ab03f5a62cd0428ecac5b80d13db6c1c153668d5
userX <userX@gmail.com>
2018-11-15 15:08:46 +0530

    ssssss

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,9 @@
 <template>
-    <div class="container">
-
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">{{ vheader }}</div>
-
-                    <div class="card-body" v-for="v in arr">
-                        {{ v }}
-                    </div>
-
-                </div>
-            </div>
-             <other-component  :h1="arr[0]"></other-component>
-        </div>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import OtherComponent from './components/InnerComponent.vue'
-import { EventBus } from './Events.js';
-export default {
-    data(){
-        return {
-            arr : [1,2,3,4,5],
-            vheader : 'q',
-        }
-        
-    },
-    methods:{
-        printhello(){
-            return "hello";
-        }, 
-        
-    },
-    mounted() {
-      this.vheader = 'Change1';
-       EventBus.$emit('init');
-    },
-    components: {
-            OtherComponent
-        }
-}
-</script>
