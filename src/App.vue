8e42bff1a2c0d9ef26886d60fd82a48cd19faff0
userX <userX@gmail.com>
2018-11-16 19:10:36 +0530

    sss

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link><span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
+      <router-link to="./">Home</router-link> |
+      <router-link to="./about">About</router-link><span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -6,53 +6,43 @@ import About from './views/About.vue'
 import Login from './components/Login.vue'
 import Secure from './components/Secure.vue'
 import Register from './components/Register.vue'
-
 Vue.use(Router)
-
 let router = new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register
-    },
-    {
-      path: '/secure',
-      name: 'secure',
-      component: Secure,
-      meta: { 
-        requiresAuth: true
-      }
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
-  ]
+    mode: 'history',
+    routes: [{
+        path: '/',
+        name: 'home',
+        component: Home
+    }, {
+        path: '/login',
+        name: 'login',
+        component: Login
+    }, {
+        path: '/register',
+        name: 'register',
+        component: Register
+    }, {
+        path: '/secure',
+        name: 'secure',
+        component: Secure,
+        meta: {
+            requiresAuth: true
+        }
+    }, {
+        path: '/about',
+        name: 'about',
+        component: About
+    }]
 })
-
 router.beforeEach((to, from, next) => {
-  if(to.matched.some(record => record.meta.requiresAuth)) {
-    if (store.getters.isLoggedIn) {
-      next()
-      return
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters.isLoggedIn) {
+            next()
+            return
+        }
+        next('/login')
+    } else {
+        next()
     }
-    next('/login') 
-  } else {
-    next() 
-  }
 })
-
 export default router
